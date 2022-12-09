7a80e6cc57f81623147b51cc7eecb6c8dbce5556
Spincer1 <spincerearin@gmail.com>
2022-12-09 13:33:06 -0800

    g

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -20,18 +20,6 @@ int main(void)
 
 			return (0);
 
-#include <stdio.h>
-
-/**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-
 }
 Then run in terminal 
-gcc 4-puts.c -o 4-puts}
+gcc 4-puts.c -o 4-puts
