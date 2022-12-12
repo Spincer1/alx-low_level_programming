e6eba3848a1af2f6dcf8bcfd36fadb79a4ed726b
Spincer1 <spincerearin@gmail.com>
2022-12-12 04:55:26 -0800

    g

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
+
+
+
 /**
- *  * main - main block
- *   * Return: 0
- *    */
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
 int main(void)
+
 {
+
 		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+
+			return (0);
+
 }
