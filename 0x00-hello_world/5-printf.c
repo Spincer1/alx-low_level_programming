7926fb29926fcd8a330f3a0ce14df886238f4aea
Spincer1 <spincerearin@gmail.com>
2022-12-21 13:14:02 -0800

    g

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- *  * main - main block
- *   * Return: 0
- *    */
+ * main - Entry point
+ *
+ * Return: 0
+ */
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
 }
