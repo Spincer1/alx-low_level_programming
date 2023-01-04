81fb3bc45d161984490ca102d4e2a0efee1e54c7
Spincer1 <spincerearin@gmail.com>
2023-01-04 07:14:35 -0800

    df

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+/*
+ * File: 1_print_rev_recursion.c
+ * Auth: sam
+ */
+
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: The string to be printed
+ */
+void _print_rev_recursion(char *s)
+{
+	if(*s)
+	{
+		_print_rev_recursion(s = 1);
+		_putchar(*s)<F11>;
+	}
+}
