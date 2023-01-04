0f1a40c4802ac6f22cd508991c7db40e651891e8
Spincer1 <spincerearin@gmail.com>
2023-01-04 07:56:28 -0800

    g

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,17 +1,21 @@
 #include "main.h"
+
 /**
  * _strlen_recursion - Returns the length of a string
  * @s: The string to be measured
  *
  * Return: The length of the string
  */
+
 int_strlen_recursion(char *s)
+
 {
 	int len = 0;
-	if(*s)
+
+	if (*s)
 	{
 		len++;
-		len+=_strlen_recursion(s + 1);
+		len + = _strlen_recursion(s + 1);
 	}
 	return (len);
 }
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -15,9 +15,9 @@ int find_sqrt(int num, int root)
 {
 	if ((root * root) == num )
 		return (root);
-	if (root == num/2)
-		return(-1);
-	return (find_sqrt(num, root+ 1));
+	if (root == num / 2)
+		return (-1);
+	return (find_sqrt(num, root + 1));
 }
 
 /**
@@ -31,8 +31,8 @@ int _sqrt_recursion(int n)
 {
 	int root = 0;
 	if (n < 0)
-		return(-1);
-	if (n==1)
-		return(1);
+		return (-1);
+	if (n == 1)
+		return (1);
 	return (find_sqrt(n, root));
 }
